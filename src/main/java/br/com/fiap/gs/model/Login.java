fa4871384cf10f21de2970de0b6cdc1981535bbb
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2024-11-18 11:59:27 -0300

    :recycle: refactor: Atualizando as entidades para refletir no banco de dados

--- src/main/java/br/com/fiap/gs/model/Login.java
+++ src/main/java/br/com/fiap/gs/model/Login.java
@@ -6,7 +6,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "OP_LOGIN")
 public class Login implements UserDetails {
@@ -19,15 +24,6 @@ public class Login implements UserDetails {
 
     @Column(name = "senha")
     private String senha;
-
-    public Login() {
-    }
-
-    public Login(Long id, String email, String senha) {
-        this.id = id;
-        this.email = email;
-        this.senha = senha;
-    }
 
     public Login(String email, String senha) {
         this.email = email;
@@ -67,29 +63,5 @@ public class Login implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
     }
 }
