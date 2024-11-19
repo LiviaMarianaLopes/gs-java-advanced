f9d66cbff6c106bcaa65f75ce5c068e5fff9e480
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2024-11-19 16:22:59 -0300

    :recycle: refactor: Atualizações nas configurações da aplicação

--- src/main/java/br/com/fiap/gs/GsApplication.java
+++ src/main/java/br/com/fiap/gs/GsApplication.java
@@ -4,14 +4,16 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "br.com.fiap.gs.repository")
 @OpenAPIDefinition(info =
 @Info(title = "API GS", version = "0.0.1"))
-public class OdontoprevApplication {
+public class GsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OdontoprevApplication.class, args);
+		SpringApplication.run(GsApplication.class, args);
 	}
 
 }
